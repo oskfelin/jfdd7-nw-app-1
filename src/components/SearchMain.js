6f3299697632634141ab85a237a0c5d4545630c2
marcin-kopystynski <marcin.kopystynski@gmail.com>
2017-07-05 16:08:49 +0200

    Search bar init.

--- src/components/SearchMain.js
+++ src/components/SearchMain.js
@@ -6,7 +6,12 @@ export default class SearchMain extends React.Component {
   render() {
     return (
       <div>
-
+        <div class="col-sm-offset-1 col-sm-10">
+          <input type="email" class="form-control" id="exampleInputEmail1" placeholder="email@example.com">
+        </div>
+        <div class="col-sm-offset-1 col-sm-10">
+          <button id="btn-game" type="submit" class="btn btn-block send">Wyślij</button>
+        </div>
 
       </div>
     )
