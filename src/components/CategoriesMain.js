a44fe3ccb291740b2b8230921d44d4a3fef3e0cd
oskfelin <oskfelin@gmail.com>
2017-07-05 16:18:53 +0200

    grid created

--- src/components/CategoriesMain.js
+++ src/components/CategoriesMain.js
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { Col } from 'react-bootstrap'
 
 export default class CategoriesMain extends React.Component {
 
+render() {
+  return(
+    <grid>
+      <Col xs={6} md={4}>Kategoria 1</Col>
+      <Col xs={6} md={4}>Kategoria 2</Col>
+      <Col xs={6} md={4}>Kategoria 3</Col>
+      <Col xs={6} md={4}>Kategoria 4</Col>
+      <Col xs={6} md={4}>Kategoria 5</Col>
+      <Col xs={6} md={4}>Kategoria 6</Col>
 
 
 
-
+    </grid>
+  )
+}
 }
