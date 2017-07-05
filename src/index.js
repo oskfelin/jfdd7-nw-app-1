9336e784058051aed3e9a469f3419ffde098c0db
JarekMichalski <yaro.michalski@gmail.com>
2017-07-05 16:16:32 +0200

    ProductPageView-init

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
-import App from './components/App'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
