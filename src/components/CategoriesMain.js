c3c23623d2820aae64b5e7941b4d2deacf08772d
oskfelin <oskfelin@gmail.com>
2017-07-06 16:31:21 +0200

    images template added to categories

--- src/components/CategoriesMain.js
+++ src/components/CategoriesMain.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {Col, Grid} from 'react-bootstrap'
+import {Col, Grid, Image} from 'react-bootstrap'
 import './Categories.css'
 
 export default class CategoriesMain extends React.Component {
@@ -9,12 +9,12 @@ export default class CategoriesMain extends React.Component {
     return (
       <Grid fluid>
         <Grid>
-          <Col xs={6} md={4} className="category"><div>Kategoria 1</div></Col>
-          <Col xs={6} md={4} className="category"><div>Kategoria 2</div></Col>
-          <Col xs={6} md={4} className="category"><div>Kategoria 3</div></Col>
-          <Col xs={6} md={4} className="category"><div>Kategoria 4</div></Col>
-          <Col xs={6} md={4} className="category"><div>Kategoria 5</div></Col>
-          <Col xs={6} md={4} className="category"><div>Kategoria 6</div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
         </Grid>
       </Grid>
     )
