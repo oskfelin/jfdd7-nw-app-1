1f6a2a2e53e95b995870e244ca1310be98393be1
Mateusz Targowski <mateusztargowski@vp.pl>
2017-07-06 16:10:01 +0200

    stworzenie plikow result i resultview

---
+++ src/components/Result.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Result.css'
+
+
+const ResultsView = (props) => (
+
+
+)
+
---
+++ src/components/ResultsView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ResultsView.css'
+
+
+const ResultsView = (props) => (
+
+
+)
+
+
+
+
