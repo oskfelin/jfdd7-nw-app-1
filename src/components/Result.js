db0345c42bcb9d3bc1834ec6ff40d655b3e557aa
Mateusz Targowski <mateusztargowski@vp.pl>
2017-07-06 16:53:14 +0200

    stworzenie szkiletu divow na pasku result

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Result.css'
 
+import {
+
+} from 'react-bootstrap'
 
 const Result = (props) => (
-  <div>Search Result</div>
+  <div className="Result">
+    <Row>
+        <div col> Search Photo </div>
 
+        <div>
+            <h1>Name</h1>
+            <button>INFO</button>
+        </div>
+
+        <div>Price</div>
+
+    </Row>
+  </div>
 
 )
 
