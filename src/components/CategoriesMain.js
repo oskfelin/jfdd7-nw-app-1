289f5399f074c3bb985258c641dc9a9ddd053b22
oskfelin <oskfelin@gmail.com>
2017-07-06 16:41:30 +0200

    images styling

--- src/components/CategoriesMain.js
+++ src/components/CategoriesMain.js
@@ -9,12 +9,12 @@ export default class CategoriesMain extends React.Component {
     return (
       <Grid fluid>
         <Grid>
-          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
-          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
-          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
-          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
-          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
-          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} thumbnail/><p>Kategoria 6</p></div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded responsive/><p>Kategoria 6</p></div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded responsive/><p>Kategoria 6</p></div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded responsive/><p>Kategoria 6</p></div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded responsive/><p>Kategoria 6</p></div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded responsive/><p>Kategoria 6</p></div></Col>
+          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded responsive/><p>Kategoria 6</p></div></Col>
         </Grid>
       </Grid>
     )
