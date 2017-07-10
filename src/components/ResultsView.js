1b2aae72b75615ccdd84110e1b720b996478a285
oskfelin <oskfelin@gmail.com>
2017-07-10 16:42:40 +0200

    fetched database

--- src/components/ResultsView.js
+++ src/components/ResultsView.js
@@ -1,19 +1,67 @@
 import React from 'react'
-import './ResultsView.css'
 
-import Result from './Result'
+import {
+  Grid,
+  Col
+} from 'react-bootstrap'
+import './Result.css'
 
-const ResultsView = (props) => (
+export default class ResultsView extends React.Component {
 
-<div>
-    <Result/>
-    <Result/>
-    <Result/>
-    <Result/>
-    <Result/>
-</div>
-)
+  state = {
+    shops: []
+  }
 
-export default ResultsView
+  componentWillMount() {
+    fetch(
+      process.env.PUBLIC_URL + '/data/shops.json'
+    ).then(
+      response => response.json()
+    ).then(
+      data => this.setState({
+        shops: data
+      })
+    ).catch(
+      error => console.log(error.message)
+    )
+  }
 
 
+  render(){
+
+
+
+    return (
+      <div className="Result">
+
+        {
+          this.state.shops.map(
+            shop => shop.products
+          ).reduce(
+            (total, next) => total.concat(next), []
+          ).map(
+            product =><Grid>
+                <Col sm={2} className="resultBorder">
+                    <div > FOTO </div>
+                </Col>
+                <Col sm={8}>
+                    <div>
+                        <h1 className="resultName">{product.name} </h1>
+
+                        <button className="resultButton">INFO</button>
+
+                    </div>
+                </Col>
+                <Col sm={2} className="resultBorder">
+                    <div>{product.price + ' zł'}</div>
+                </Col>
+
+            </Grid>
+          )
+        }
+
+      </div>
+
+    )
+  }
+}
