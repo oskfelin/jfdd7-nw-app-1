d85734a7f514c689c1fce22d85ad561905b74cbe
oskfelin <oskfelin@gmail.com>
2017-07-11 15:21:06 +0200

    redux, redux-thunk installed

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import { Provider } from 'react-redux'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
 
 ReactDOM.render(
+  <Provider store={store}>
   <App />,
+  </Provider>
   document.getElementById('root')
 )
 registerServiceWorker()
