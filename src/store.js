712987b29ac27721026d74c8d04b0c81747985e6
oskfelin <oskfelin@gmail.com>
2017-07-11 15:23:03 +0200

    store.js created

--- src/store.js
+++ src/store.js
@@ -1,7 +1,20 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
 
 
+const reducer = combineReducers({
 
+
+})
+
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+  applyMiddleware(
+    thunk
+  )
+))
 
 export default store
