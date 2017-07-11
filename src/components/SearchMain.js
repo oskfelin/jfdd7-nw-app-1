6d64fb0fc91c579faaaedcc5d5110ab87298f3e1
marcin-kopystynski <marcin.kopystynski@gmail.com>
2017-07-11 16:51:52 +0200

    search engine works :)

--- src/components/SearchMain.js
+++ src/components/SearchMain.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import {
   FormGroup,
   FormControl,
@@ -11,16 +12,33 @@ import {
 } from 'react-bootstrap'
 
 import './SearchMain.css'
+import { updateSearchPhrase } from '../state/searchEngine'
 
-export default class SearchMain extends React.Component {
 
+export default connect(
+  state => ({
+    shops: state.shops,
+    searchPhrase: state.searchEngine.searchPhrase
+  }),
+  dispatch => ({
+    updateSearchPhrase: (event) => dispatch(updateSearchPhrase(event.target.value))
+  })
+)(
+
+ class SearchMain extends React.Component {
   render() {
     return (
 <Grid>
       <Col mdOffset={2} md={8} >
       <FormGroup >
         <InputGroup >
-          <FormControl className="search-field" type="text" bsSize="large" placeholder="Czego potrzebujesz?" />
+          <FormControl
+            className="search-field"
+            type="text"
+            bsSize="large"
+            placeholder="Czego potrzebujesz?"
+            onChange={this.props.updateSearchPhrase}
+          />
 
           <DropdownButton
             componentClass={InputGroup.Button}
@@ -33,7 +51,7 @@ export default class SearchMain extends React.Component {
             <MenuItem key="2">Kategoria2</MenuItem>
             <MenuItem key="3">Kategoria3</MenuItem>
             <MenuItem key="4">Kategoria4</MenuItem>
-            <MenuItem key="4">Kategoria5</MenuItem>
+            <MenuItem key="5">Kategoria5</MenuItem>
           </DropdownButton>
           <InputGroup.Button>
             <Button bsSize="large"><img alt="" src={process.env.PUBLIC_URL + '/images/magnifier.png'} height="20"/></Button>
@@ -46,5 +64,5 @@ export default class SearchMain extends React.Component {
     )
   }
 }
-
+)
 
