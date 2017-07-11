0585f59f6298b648adb14d4df4f7b5307061e11c
Mateusz Targowski <mateusztargowski@vp.pl>
2017-07-11 15:36:12 +0200

    Structure change

--- src/components/ResultsView.js
+++ src/components/ResultsView.js
@@ -1,19 +1,66 @@
 import React from 'react'
+import uniqBy from 'lodash.uniqby'
+
+import {
+  Grid,
+  Col
+} from 'react-bootstrap'
 import './ResultsView.css'
 
-import Result from './Result'
+export default class ResultsView extends React.Component {
 
-const ResultsView = (props) => (
+  state = {
+    shops: []
+  }
 
-<div>
-    <Result/>
-    <Result/>
-    <Result/>
-    <Result/>
-    <Result/>
-</div>
-)
+  componentWillMount() {
+    fetch(
+      process.env.PUBLIC_URL + '/data/shops.json'
+    ).then(
+      response => response.json()
+    ).then(
+      data => this.setState({
+        shops: data
+      })
+    ).catch(
+      error => console.log(error.message)
+    )
+  }
 
-export default ResultsView
+
+  render(){
 
 
+
+    return (
+      <div className="Result">
+
+        {
+          uniqBy(this.state.shops.map(
+            shop => shop.products
+          ).reduce(
+            (total, next) => total.concat(next), []
+          ), 'name').map(
+            product =><Grid>
+                <Col sm={2} className="resultBorder">
+                    <div > FOTO </div>
+                </Col>
+                <Col sm={8}>
+                    <div>
+                        <h1 className="resultName">{product.name} </h1>
+                    </div>
+                </Col>
+                <Col sm={2} className="resultBorder">
+                    <div>{product.price + ' zł'}</div>
+                     <button className="resultButton">INFO</button>
+                </Col>
+
+            </Grid>
+          )
+        }
+
+      </div>
+
+    )
+  }
+}
