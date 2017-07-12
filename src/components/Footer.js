c67973b7418d3a8bcd62258bba696f268a90c1fa
Oskar Feliński <oskfelin@gmail.com>
2017-07-12 15:06:22 +0200

    Revert "footer - errors fixed."

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,30 +13,32 @@ export default class Footer extends React.Component {
         <div className="container">
           <div className="text-center container-list">
             <Col xs={6}>
-              <a className="hvr-underline-from-left footer-list">Reklama w serwisie</a>
+              <a className="hvr-underline-from-left footer-list" href="#">Reklama w serwisie</a>
             </Col>
             <Col xs={6}>
-              <a className="hvr-underline-from-left footer-list">Polityka prywatności</a>
+              <a className="hvr-underline-from-left footer-list" href="#">Polityka prywatności</a>
             </Col>
             <Col xs={6}>
-              <a className="hvr-underline-from-left footer-list">Regulamin serwisu</a>
+              <a className="hvr-underline-from-left footer-list" href="#">Regulamin serwisu</a>
             </Col>
             <Col xs={6}>
-              <a className="hvr-underline-from-left footer-list">Kontakt</a>
+              <a className="hvr-underline-from-left footer-list" href="#">Kontakt</a>
             </Col>
 
           </div>
+          <p className="text-center find-us">Znajdziesz nas na:</p>
+
           <div
             className="col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-3 col-md-6 col-lg-offset-4 col-lg-4 text-center social-media img-responsive">
 
-            <a href="http://www.facebook.com"  target="_blank" rel="noreferrer noopener" ><img
-              src={process.env.PUBLIC_URL + 'images/footer/fb.svg'} alt="facebook"  height="48"/></a>
-            <a href="http://www.plus.google.com"  target="_blank" rel="noreferrer noopener" ><img
-              src={process.env.PUBLIC_URL + 'images/footer/googleplus.svg'} alt="googleplus" height="48"/></a>
-            <a href="http://www.twitter.com"  target="_blank" rel="noreferrer noopener" ><img
-              src={process.env.PUBLIC_URL + 'images/footer/twitter.svg'} alt="twitter" height="48"/></a>
-            <a href="http://www.youtube.com"  target="_blank" rel="noreferrer noopener" ><img
-              src={process.env.PUBLIC_URL + 'images/footer/youtube.svg'} alt="youtube" height="48"/></a>
+            <a href="http://www.facebook.com" target="_blank"><img
+              src={process.env.PUBLIC_URL + 'images/footer/fb.svg'} height="48"/></a>
+            <a href="http://www.facebook.com" target="_blank"><img
+              src={process.env.PUBLIC_URL + 'images/footer/googleplus.svg'} height="48"/></a>
+            <a href="http://www.facebook.com" target="_blank"><img
+              src={process.env.PUBLIC_URL + 'images/footer/twitter.svg'} height="48"/></a>
+            <a href="http://www.facebook.com" target="_blank"><img
+              src={process.env.PUBLIC_URL + 'images/footer/youtube.svg'} height="48"/></a>
 
           </div>
           <div className="col-xs-12 text-center">
