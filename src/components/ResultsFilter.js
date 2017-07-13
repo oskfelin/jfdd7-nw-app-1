81fe20cdffec4cca3cea51f17eee8d33f75a2e82
Marcin Kopystynski <marcin.kopystynski@gmail.com>
2017-07-13 20:48:08 +0200

    filtorwanie

--- src/components/ResultsFilter.js
+++ src/components/ResultsFilter.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
-// import { Link } from 'react-router-dom'
-// import {
-//   Grid,
-//   Col
-// } from 'react-bootstrap'
+import {
+  ButtonGroup,
+  DropdownButton,
+  MenuItem
+} from 'react-bootstrap'
 
 import { fetchShops } from '../state/shops'
+import { activeFilter } from '../state/searchEngine'
 
 
+
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 
 export default connect(
   state => ({
     shops: state.shops,
-    searchPhrase: state.searchEngine.searchPhrase,
+
     activeFilter: state.searchEngine.activeFilterName
   }),
   dispatch => ({
-    fetchShops: () => dispatch(fetchShops())
+    fetchShops: () => dispatch(fetchShops()),
+    activateFilter: (key) => dispatch(activeFilter(key))
   })
 )(
 
@@ -30,12 +35,38 @@ export default connect(
     }
 
     render(){
-      // const { data, fetching, error } = this.props.shops
       return (
-        <div className="ResultFilter">
-            123
+        <div className="ResultFilter" >
+          <ButtonGroup vertical block>
+            <DropdownButton title="Producent"
+                            id="bg-vertical-dropdown-1"
+                            onClick={() => this.props.activeFilter}> ??????????????
 
+              <MenuItem eventKey="1">Dropdown link</MenuItem>
+              <MenuItem eventKey="2">Dropdown link</MenuItem>
+            </DropdownButton>
+            <DropdownButton title="Sklep" id="bg-vertical-dropdown-2">
+              <MenuItem eventKey="1">Dropdown link</MenuItem>
+              <MenuItem eventKey="2">Dropdown link</MenuItem>
+            </DropdownButton>
+            <DropdownButton title="Aparat" id="bg-vertical-dropdown-3">
+              <MenuItem eventKey="1">Dropdown link</MenuItem>
+              <MenuItem eventKey="2">Dropdown link</MenuItem>
+            </DropdownButton>
+            <DropdownButton title="Dodatkowa karta pamięci" id="bg-vertical-dropdown-4">
+              <MenuItem eventKey="1">Dropdown link</MenuItem>
+              <MenuItem eventKey="2">Dropdown link</MenuItem>
+            </DropdownButton>
+            <DropdownButton title="Aparat" id="bg-vertical-dropdown-5">
+              <MenuItem eventKey="1">Dropdown link</MenuItem>
+              <MenuItem eventKey="2">Dropdown link</MenuItem>
+            </DropdownButton>
+          </ButtonGroup>
 
+          <div className="slider">
+            <p>Zakres cenowy</p>
+          <Range defaultValue={[20, 80]}/>
+          </div>
         </div>
 
       )
