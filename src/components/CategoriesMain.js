eab83923df3b4a39cca5f46dcd5181040a0c9bca
oskfelin <oskfelin@gmail.com>
2017-07-14 15:19:36 +0200

    cathegory on main page now has a link

--- src/components/CategoriesMain.js
+++ src/components/CategoriesMain.js
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import {Col, Grid, Image, Label} from 'react-bootstrap'
 import './Categories.css'
+
+
 
 export default class CategoriesMain extends React.Component {
 
@@ -9,7 +11,7 @@ export default class CategoriesMain extends React.Component {
     return (
       <Grid fluid>
         <Grid>
-          <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded responsive/><h1><Label>Kategoria 6</Label></h1></div></Col>
+          <Col xs={12} sm={6} lg={4}><Link to="/result-view"><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded responsive/><h1><Label>Kategoria 6</Label></h1></div></Link></Col>
           <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded responsive/><h1><Label>Kategoria 6</Label></h1></div></Col>
           <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded responsive/><h1><Label>Kategoria 6</Label></h1></div></Col>
           <Col xs={12} sm={6} lg={4}><div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded responsive/><h1><Label>Kategoria 6</Label></h1></div></Col>
