3189fa150e7caf2cc6fbfd7f4ff0b4bbe14ef4f3
Marcin Kopystynski <marcin.kopystynski@gmail.com>
2017-07-16 16:37:42 +0200

    added filters: producent, camera, slot sd, size

--- src/components/ResultsFilter.js
+++ src/components/ResultsFilter.js
@@ -43,27 +43,52 @@ export default connect(
 
               <MenuItem eventKey="name_xiaomi">Xiaomi</MenuItem>
               <MenuItem eventKey="name_lenovo">Lenovo</MenuItem>
-              <MenuItem eventKey="3">HTC</MenuItem>
-              <MenuItem eventKey="4">Iphone</MenuItem>
-              <MenuItem eventKey="5">LG</MenuItem>
-              <MenuItem eventKey="6">Samsung</MenuItem>
-              <MenuItem eventKey="7">Huawei</MenuItem>
+              <MenuItem eventKey="name_htc">HTC</MenuItem>
+              <MenuItem eventKey="name_iphone">Iphone</MenuItem>
+              <MenuItem eventKey="name_lg">LG</MenuItem>
+              <MenuItem eventKey="name_samsung">Samsung</MenuItem>
+              <MenuItem eventKey="name_huawei">Huawei</MenuItem>
             </DropdownButton>
-            <DropdownButton title="Sklep" id="bg-vertical-dropdown-2">
-              <MenuItem eventKey="1">Dropdown link</MenuItem>
+
+            <DropdownButton title="Sklep"
+                            id="bg-vertical-dropdown-2"
+                            onSelect={key => this.props.activateFilter(key)} >
+              <MenuItem eventKey="name_biedronka">Biedronka</MenuItem>
+              <MenuItem eventKey="name_lidl">Lidl</MenuItem>
               <MenuItem eventKey="2">Dropdown link</MenuItem>
             </DropdownButton>
-            <DropdownButton title="Aparat" id="bg-vertical-dropdown-3">
-              <MenuItem eventKey="1">Dropdown link</MenuItem>
-              <MenuItem eventKey="2">Dropdown link</MenuItem>
+
+
+
+            <DropdownButton title="Aparat"
+                            id="bg-vertical-dropdown-3"
+                            onSelect={key => this.props.activateFilter(key)} >
+              <MenuItem eventKey="camera_3p2">3.2 mpx</MenuItem>
+              <MenuItem eventKey="camera_4">4 mpx</MenuItem>
+              <MenuItem eventKey="camera_6">6 mpx</MenuItem>
+              <MenuItem eventKey="camera_6p4">6.4 mpx</MenuItem>
+              <MenuItem eventKey="camera_12">12 mpx</MenuItem>
+              <MenuItem eventKey="camera_24">24 mpx</MenuItem>
             </DropdownButton>
-            <DropdownButton title="Dodatkowa karta pamięci" id="bg-vertical-dropdown-4">
-              <MenuItem eventKey="1">Dropdown link</MenuItem>
-              <MenuItem eventKey="2">Dropdown link</MenuItem>
+
+
+            <DropdownButton title="Dodatkowa karta pamięci"
+                            id="bg-vertical-dropdown-4"
+                            onSelect={key => this.props.activateFilter(key)}>
+              <MenuItem eventKey="slot_sd">Tak</MenuItem>
+              <MenuItem eventKey="no_slot">Nie</MenuItem>
             </DropdownButton>
-            <DropdownButton title="Aparat" id="bg-vertical-dropdown-5">
-              <MenuItem eventKey="1">Dropdown link</MenuItem>
-              <MenuItem eventKey="2">Dropdown link</MenuItem>
+
+
+
+            <DropdownButton title="Przekątna ekranu (cale)" id="bg-vertical-dropdown-5">
+              <MenuItem eventKey="size_3p9">3.9</MenuItem>
+              <MenuItem eventKey="size_4p2">4.2</MenuItem>
+              <MenuItem eventKey="size_4p5">4.5</MenuItem>
+              <MenuItem eventKey="size_4p8">4.8</MenuItem>
+              <MenuItem eventKey="size_5p1">5.1</MenuItem>
+              <MenuItem eventKey="size_5p2">5.2</MenuItem>
+              <MenuItem eventKey="size_6">6</MenuItem>
             </DropdownButton>
           </ButtonGroup>
 
