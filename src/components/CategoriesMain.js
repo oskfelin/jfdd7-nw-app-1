dc3b644b73625404e3bea0b16d88d1c3c981b7d6
Marcin Kopystynski <marcin.kopystynski@gmail.com>
2017-07-17 22:09:04 +0200

    styling main page - version 0.0001.

--- src/components/CategoriesMain.js
+++ src/components/CategoriesMain.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {Col, Grid, Image, Label} from 'react-bootstrap'
 import './Categories.css'
+
 import {activeFilter} from '../state/searchEngine'
 import {connect} from 'react-redux'
 export default connect(
@@ -19,7 +20,7 @@ export default connect(
       return (
         <Grid fluid>
           <Grid>
-            <Col xs={12} sm={6} lg={4}>
+             <Col xs={12} sm={6} lg={4}>
               <Link
                 title={this.props.activeFilterName}
                 onClick={() => this.props.activateFilter('smartphones')}
@@ -33,24 +34,39 @@ export default connect(
               </Link>
             </Col>
             <Col xs={12} sm={6} lg={4}>
-              <div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded
-                                               responsive/><h1><Label>Comming soon</Label></h1></div>
+              <div className="category">
+                <Image responsive src={process.env.PUBLIC_URL + '/images/main_kids.jpg'} rounded/>
+                <Image className="coming-icon" responsive src={process.env.PUBLIC_URL + '/images/comingsoon.png'} rounded/>
+                <p className="coming-soon">W BUDOWIE <br /> ZAPRASZAMY WKRÓTCE!</p>
+                <h1><Label>Dziecko</Label></h1></div>
             </Col>
             <Col xs={12} sm={6} lg={4}>
-              <div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded
-                                               responsive/><h1><Label>Comming soon</Label></h1></div>
+              <div className="category">
+                <Image responsive src={process.env.PUBLIC_URL + '/images/main_cars.jpg'} rounded/>
+                <Image className="coming-icon" responsive src={process.env.PUBLIC_URL + '/images/comingsoon.png'} rounded/>
+                <p className="coming-soon">W BUDOWIE <br /> ZAPRASZAMY WKRÓTCE!</p>
+                <h1><Label>Samochody</Label></h1></div>
             </Col>
             <Col xs={12} sm={6} lg={4}>
-              <div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded
-                                               responsive/><h1><Label>Comming soon</Label></h1></div>
+              <div className="category">
+                <Image  responsive src={process.env.PUBLIC_URL + '/images/main_moda.jpg'} rounded/>
+                <Image className="coming-icon" responsive src={process.env.PUBLIC_URL + '/images/comingsoon.png'} rounded/>
+                <p className="coming-soon">W BUDOWIE <br /> ZAPRASZAMY WKRÓTCE!</p>
+                <h1><Label>Moda</Label></h1></div>
             </Col>
             <Col xs={12} sm={6} lg={4}>
-              <div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded
-                                               responsive/><h1><Label>Comming soon</Label></h1></div>
+              <div className="category">
+                <Image responsive src={process.env.PUBLIC_URL + '/images/main_sport.jpg'} rounded/>
+                <Image className="coming-icon" responsive src={process.env.PUBLIC_URL + '/images/comingsoon.png'} rounded/>
+                <p className="coming-soon">W BUDOWIE <br /> ZAPRASZAMY WKRÓTCE!</p>
+                <h1><Label>Sport</Label></h1></div>
             </Col>
             <Col xs={12} sm={6} lg={4}>
-              <div className="category"><Image src={process.env.PUBLIC_URL + '/images/MobilePhone.jpg'} rounded
-                                               responsive/><h1><Label>Comming soon</Label></h1></div>
+              <div className="category">
+                <Image responsive src={process.env.PUBLIC_URL + '/images/main_uroda.jpg'} rounded/>
+                <Image className="coming-icon" responsive src={process.env.PUBLIC_URL + '/images/comingsoon.png'} rounded/>
+                <p className="coming-soon">W BUDOWIE <br /> ZAPRASZAMY WKRÓTCE!</p>
+                <h1><Label className="label">Uroda</Label></h1></div>
             </Col>
           </Grid>
         </Grid>
