18c8cbee2d23d791252b8a063649218f9c2e0ac7
marcin-kopystynski <marcin.kopystynski@gmail.com>
2017-07-21 15:28:01 +0200

    firebase installed

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
 import store from './store'
+import firebase from 'firebase'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDztWpOCDHq0dJ5ukwGTX1Afs8gzeO6RLg",
+  authDomain: "jfdd7-nw-app.firebaseapp.com",
+  databaseURL: "https://jfdd7-nw-app.firebaseio.com",
+  projectId: "jfdd7-nw-app",
+  storageBucket: "",
+  messagingSenderId: "385172902941"
+})
+
 
 ReactDOM.render(
   <Provider store={store}>
