e8435115fcaa037bb26631088394b03cf93b8b3b
JarekMichalski <yaro.michalski@gmail.com>
2017-07-24 20:16:04 +0200

    ComparePreview connected to comparedProducts

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,28 @@ import React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import ProductPageView from "./ProductPageView"
 import CategoriesMain from './CategoriesMain'
+import Comparison from './Comparison'
+import ComparePreview from './ComparePreview'
 import Home from './Home'
 
 import ResultView from './ResultsView'
 import SearchMain from './SearchMain'
 import Footer from './Footer'
 
-import Comparison from './Comparison'
 export default class App extends React.Component {
 
   render() {
     return (
       <Router>
         <div>
-       <Home/>
-        <SearchMain />
+          <Home/>
+          <ComparePreview/>
+          <SearchMain/>
           <Route path="/comparison" component={Comparison}/>
-        <Route path="/result-view" component={ResultView}/>
-        <Route exact path="/" component={CategoriesMain}/>
-        <Route path="/product-page-view/:productName" component={ProductPageView}/>
-        <Footer />
+          <Route path="/result-view" component={ResultView}/>
+          <Route exact path="/" component={CategoriesMain}/>
+          <Route path="/product-page-view/:productName" component={ProductPageView}/>
+          <Footer/>
         </div>
       </Router>
     )
--- src/components/ComparePreview.js
+++ src/components/ComparePreview.js
@@ -1,21 +1,46 @@
-import { nameOfAnimation as Menu } from 'react-burger-menu'
+import React from 'react'
+import {connect} from 'react-redux'
+import {Table} from 'react-bootstrap'
+import {toggle} from '../state/comparedProducts'
 
-class Example extends React.Component {
-  showSettings (event) {
-    event.preventDefault();
-  .
-  .
-  .
-  }
+export default connect(
+  state => ({
+    productsIds: state.comparedProducts.productsIds,
+    shops: state.shops
+  }),
+  dispatch => ({
+    toggleCompare: id => dispatch(toggle(id)),
 
-  render () {
-    return (
-      <Menu>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-      </Menu>
-    );
+  })
+)(
+  class ComparePreview extends React.Component {
+
+    render() {
+      const {data} = this.props.shops
+      const dataToDisplay = data === null ? [] : data.map(
+        shop => shop.products
+      ).reduce(
+        (total, next) => total.concat(next), []
+      )
+      console.log(dataToDisplay)
+      return (
+        <div>
+          <Table>
+            {
+              dataToDisplay.filter(
+                product => this.props.productsIds.includes(product.id)
+              ).map(
+                product =>
+                  <tbody>
+                  <tr>
+                    <td>{product.name}</td>
+                  </tr>
+
+                  </tbody>
+              )}
+          </Table>
+        </div>
+      )
+    }
   }
-}
+)
