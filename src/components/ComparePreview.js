7262a8a2a9f6432f3cd75629983c11841073b239
JarekMichalski <yaro.michalski@gmail.com>
2017-07-24 20:53:54 +0200

    Added buttons

--- src/components/ComparePreview.js
+++ src/components/ComparePreview.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Table} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import {Grid, Row, Col, Panel, Button} from 'react-bootstrap'
 import {toggle} from '../state/comparedProducts'
 
 export default connect(
@@ -23,21 +24,32 @@ export default connect(
       )
       console.log(dataToDisplay)
       return (
-        <div>
-          <Table>
-            {
-              dataToDisplay.filter(
-                product => this.props.productsIds.includes(product.id)
-              ).map(
-                product =>
-                  <tbody>
-                  <tr>
-                    <td>{product.name}</td>
-                  </tr>
-                  </tbody>
-              )}
-          </Table>
-        </div>
+        <Grid>
+          <Panel>
+            <Row>
+              {
+                dataToDisplay.filter(
+                  product => this.props.productsIds.includes(product.id)
+                ).map(
+                  product =>
+                    <Col xs={3}>
+                      <img width={50} alt=""
+                           src={process.env.PUBLIC_URL + '/images/smartphones/' + product.name + '.jpg'}/>
+                      <div>{product.name}</div>
+                      <Button onClick={event => {
+                        this.props.toggleCompare(product.id)
+                        event.preventDefault()
+                      }}>
+                        Nie porównuj
+                      </Button>
+                    </Col>
+                )}
+              <Button>
+                <Link to="/comparison">Przejdź do porównywarki</Link>
+              </Button>
+            </Row>
+          </Panel>
+        </Grid>
       )
     }
   }
