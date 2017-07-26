91eac3648360afa1d8c3e62a0ebf3d6d7aed1c53
Mateusz Targowski <mateusztargowski@vp.pl>
2017-07-26 16:06:40 +0200

    cleaning

--- src/components/ResultsView.js
+++ src/components/ResultsView.js
@@ -22,8 +22,7 @@ export default connect(
     price: state.searchFilters.price,
   }),
   dispatch => ({
-    toggleCompare: id => dispatch(toggle(id)),
-
+    toggleCompare: id => dispatch(toggle(id))
   })
 )(
   class ResultsView extends React.Component {
