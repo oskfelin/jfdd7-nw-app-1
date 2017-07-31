9fd72421f0a593918c8ae4996ee645aa6895005c
oskfelin <oskfelin@gmail.com>
2017-07-31 13:13:30 +0200

    Bugs fixes

--- src/components/ProductPageView.js
+++ src/components/ProductPageView.js
@@ -4,7 +4,7 @@ import uniqBy from 'lodash.uniqby'
 import {Grid, Col, Row, Carousel, Tab, Tabs, Panel, Table, Button} from 'react-bootstrap'
 import {fetchShops} from '../state/shops'
 import './ProductPageView.css'
-import {toggle } from '../state/comparedProducts'
+import {toggle} from '../state/comparedProducts'
 
 export default connect(
   state => ({
@@ -36,22 +36,25 @@ export default connect(
                   shop => shop.products
                 ).reduce(
                   (total, next) => total.concat(next), []
-                ), 'screenSize', 'camera', 'memory', 'slotSd').filter(
+                ), 'name', 'screenSize', 'camera', 'memory', 'slotSD').filter(
                   product => product.name === this.props.match.params.productName
                 ).map(
                   product =>
-              <Carousel>
-                <Carousel.Item>
-                  <img width={500} alt="" src={process.env.PUBLIC_URL + '/images/smartphones/'+product.name+'.jpg'}/>
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img width={500} alt="" src={process.env.PUBLIC_URL + '/images/smartphones/'+product.name+'.jpg'}/>
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img width={500} alt="" src={process.env.PUBLIC_URL + '/images/smartphones/'+product.name+'.jpg'}/>
-                </Carousel.Item>
-              </Carousel>
-                  )}
+                    <Carousel key={product.id}>
+                      <Carousel.Item>
+                        <img width={500} alt=""
+                             src={process.env.PUBLIC_URL + '/images/smartphones/' + product.name + '.jpg'}/>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img width={500} alt=""
+                             src={process.env.PUBLIC_URL + '/images/smartphones/' + product.name + '.jpg'}/>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img width={500} alt=""
+                             src={process.env.PUBLIC_URL + '/images/smartphones/' + product.name + '.jpg'}/>
+                      </Carousel.Item>
+                    </Carousel>
+                )}
             </Col>
             { error === null ? null : <p>{error.message}</p> }
             { fetching === false ? null : <p>Fetching data...</p>}
@@ -64,39 +67,44 @@ export default connect(
                         shop => shop.products
                       ).reduce(
                         (total, next) => total.concat(next), []
-                      ), 'screenSize', 'camera', 'memory', 'slotSd').filter(
+                      ), 'name', 'screenSize', 'camera', 'memory', 'slotSD').filter(
                         product => product.name === this.props.match.params.productName
                       ).map(
                         product =>
-                          <tbody>
+                          <tbody key={product.id}>
                           <tr>
                             <td>
-                          <Button className="ButtonCompare" onClick={event => {
-                            this.props.toggleCompare(product.id)
-                            event.preventDefault()
-                          }}>
-                            Porównaj
-                          </Button></td>
+                              <Button className="ButtonCompare" onClick={event => {
+                                this.props.toggleCompare(product.id)
+                                event.preventDefault()
+                              }}>
+                                Porównaj
+                              </Button></td>
                           </tr>
                           <tr>
                             <td>Przekątna ekranu: {product.screenSize} "</td>
                           </tr>
                           <tr>
-                            <td>Wbudowany aparat cyfrowy: {product.camera} Mpix</td>
+                            <td>Aparat cyfrowy: {product.camera} Mpix</td>
                           </tr>
                           <tr>
                             <td>Wbudowana pamięć: {product.memory} GB</td>
                           </tr>
                           <tr>
-                            <td>Obsługa kart pamięci: {product.slotSd}</td>
+                            <td>Obsługa kart pamięci: {product.slotSD}</td>
                           </tr>
                           </tbody>
                       )}
                   </Table>
                 </Tab>
                 <Tab eventKey={2} title="Opis produktu">
-                  Tab 2 content Tab 2 contentTab 2 contentTab 2 contentTab 2 contentTab 2 contentTab 2 contentTab 2
-                  content Tab 1 content Tab 1 content Tab 1 content Tab 1 content Tab 1 content Tab 1 contentTab 1
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                  laborum.
                 </Tab>
               </Tabs>
             </Col>
@@ -104,14 +112,14 @@ export default connect(
               <p className="oferts">Najlepsze oferty znalezione przez nasz serwis</p>
               {
                 data !== null && data.map(
-                  shop => shop.products.map(product => ({...product, shopName: shop.name, shopCity: shop.city }))
+                  shop => shop.products.map(product => ({...product, shopName: shop.name, shopCity: shop.city}))
                 ).reduce(
                   (total, next) => total.concat(next), []
                 ).filter(
                   product => product.name === this.props.match.params.productName
-                ).sort((a,b) => a.price > b.price).map(
+                ).sort((a, b) => a.price > b.price).map(
                   product =>
-                    <Panel>
+                    <Panel key={product.id}>
                       <Col xs={4}>
                         <div>Nazwa sklepu<br/>{product.shopName}</div>
                       </Col>
@@ -119,8 +127,8 @@ export default connect(
                         <div>Lokalizacja<br/>{product.shopCity}</div>
                       </Col>
                       <Col xs={4}>
-                      <div>Cena<br/>{product.price + ' zł'}</div>
-                    </Col>
+                        <div>Cena<br/>{product.price + ' zł'}</div>
+                      </Col>
                     </Panel>
                 )}
             </Col>
